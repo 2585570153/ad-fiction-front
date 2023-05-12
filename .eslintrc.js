6ac15d0d692c610f707baedeaa1bc52d22caf2b2
2585570153 <2585570153@qq.com>
2023-05-12 08:52:27 +0800

    导航栏开发

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        'vue/multi-word-components-names':0,//不再强制要求组件命名
+    },
+}
---
+++ src/views/Layout/components/News.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="new">
+  12349
+</div>
+</template>
+
+<script>
+export default {
+  name: "New"
+}
+</script>
+
+<style scoped>
+.new {
+  background-color: rgba(224, 158, 158, 0.1);
+  border-radius: 10px;
+  border: 0;
+  padding: 10px;
+}
+</style>

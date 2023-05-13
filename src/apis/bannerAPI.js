f0674699dac3a002cb7c446d5ecfea99f927a07f
2585570153 <2585570153@qq.com>
2023-05-13 12:22:15 +0800

    更新banner交互

---
+++ src/apis/bannerAPI.js
@@ -0,0 +1,4 @@
+import httpInstance from "@/utils/http";
+export function getCategory(){
+    return httpInstance({url: 'home/category/head'})
+}
---
+++ src/views/Layout/components/Boutique.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Boutique"
+}
+</script>
+
+<style scoped>
+
+</style>
